2e72232426d0b957d0e5f0ad4cc337b13e43b36e
lufengkai <zhanjiangmrlu@163.com>
2021-11-27 20:49:37 +0800

    feat: 博客详情

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -16,40 +16,36 @@ const getList = (author, keyword) => {
 }
 
 // 详情
-const getDetail = (id) => {
-	return {
-		id: 1,
-		title: '标题1',
-		content: '内容1',
-		createTime: 1637477628420,
-		author: '靓妹',
-	}
+const getDetail = id => {
+	const sql = `select * from blogs where id='${id}'`
+
+	return exec(sql).then(rows => rows[0])
 }
 
 // 新增
 const newBlog = (blogData = {}) => {
-  return {
-    id: 3
-  }
+	return {
+		id: 3,
+	}
 }
 
 // 更新
 const updatedBlog = (id, blogData) => {
-  // id 就是要更新博客的 ID
-  // blogData 是一个博客对象，包含 title content 属性
+	// id 就是要更新博客的 ID
+	// blogData 是一个博客对象，包含 title content 属性
 
-  return true
+	return true
 }
 
 // 删除
-const delBlog = (id) => {
-  return true
+const delBlog = id => {
+	return true
 }
 
 module.exports = {
 	getList,
 	getDetail,
-  newBlog,
-  updatedBlog,
-  delBlog
+	newBlog,
+	updatedBlog,
+	delBlog,
 }
--- src/router/blog.js
+++ src/router/blog.js
@@ -1,58 +1,62 @@
 const {
-  getList, getDetail, newBlog, updatedBlog, delBlog
+	getList,
+	getDetail,
+	newBlog,
+	updatedBlog,
+	delBlog,
 } = require('../controller/blog')
 const { SucessModel, ErrorModel } = require('../model/resModel')
 
-const handleBlogRouter = (req, res)=> {
-  const { method, url, query, path } = req
-  const { id = '' } = query || {}
-  const API = '/api/blog'
+const handleBlogRouter = (req, res) => {
+	const { method, url, query, path } = req
+	const { id = '' } = query || {}
+	const API = '/api/blog'
 
-  // 博客列表
-  if (method === 'GET' && path === `${API}/list`){
-    const { author = '', keyword = '' } = query || {}
-    const result = getList(author, keyword)
+	// 博客列表
+	if (method === 'GET' && path === `${API}/list`) {
+		const { author = '', keyword = '' } = query || {}
+		const result = getList(author, keyword)
 
-    return result.then(res => {
-      return new SucessModel(res)
-    })
-  }
+		return result.then(res => {
+			return new SucessModel(res)
+		})
+	}
 
-  // 博客详情
-  if (method === 'GET' && path === `${API}/detail`) {
-    const result = getDetail(id)
+	// 博客详情
+	if (method === 'GET' && path === `${API}/detail`) {
+		const result = getDetail(id)
 
-    return new SucessModel(result)
-  }
+		return result.then(data => new SucessModel(data))
+	}
 
-  // 新增博客
-  if (method === 'POST' && path === `${API}/new`) {
-    const data = newBlog(req.body)
+	// 新增博客
+	if (method === 'POST' && path === `${API}/new`) {
+		const data = newBlog(req.body)
 
-    return new SucessModel(data)
-  }
+		return new SucessModel(data)
+	}
 
-  // 更新博客
-  if (method === 'POST' && path === `${API}/updated`) {
-    const result = updatedBlog(req.body)
+	// 更新博客
+	if (method === 'POST' && path === `${API}/updated`) {
+		const result = updatedBlog(req.body)
 
-    if (result && id) {
-      return new SucessModel(result)
-    } else {
-      return new ErrorModel('更新博客失败')
-    }
-  }
+		if (result && id) {
+			return new SucessModel(result)
+		} else {
+			return new ErrorModel('更新博客失败')
+		}
+	}
 
-  // 删除博客
-  if (method === 'POST' && path === `${API}/del`) {
-    const result = delBlog(req.body)
+	// 删除博客
+	if (method === 'POST' && path === `${API}/del`) {
+		const result = delBlog(req.body)
 
-    if (result && id) {
-      return new SucessModel(result)
-    } else {
-      return new ErrorModel('删除博客失败')
-    }
-  }
+		if (result && id) {
+			return new SucessModel(result)
+		} else {
+			return new ErrorModel('删除博客失败')
+		}
+	}
 }
 
 module.exports = handleBlogRouter
