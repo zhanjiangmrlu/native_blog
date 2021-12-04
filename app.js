ded80a0962eeeb98e0005dd66798c3d1de9ecc47
lufengkai <zhanjiangmrlu@163.com>
2021-12-04 20:05:30 +0800

    feat: cookie登录验证

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+// session 数据
+const SESSION_DATA = {}
+
+// 获取 cookie 的过期时间
+const getCookieExpires = () => {
+	const d = new Date()
+	d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+	// console.log('cookie的过期时间:', d.toGMTString())
+	return d.toGMTString()
+}
+
 // 处理 post data
 const getPostData = req => {
 	const promise = new Promise((resolve, reject) => {
@@ -39,6 +50,41 @@ const handleServer = (req, res) => {
 	// 解析 query
 	req.query = querystring.parse(url.split('?')[1])
 
+	// 解析 cookie
+	req.cookie = {}
+	const cookieStr = req.headers.cookie || '' // k1=v1;k2=v2;k3=v3
+	console.log('cookieStr',cookieStr);
+	cookieStr.split(';').forEach(item => {
+		if (!item) return
+		const arr = item.split('=')
+		const key = arr[0].trim()
+		const val = arr[1].trim()
+		req.cookie[key] = val
+		console.log('req.cookie', req.cookie);
+	})
+
+	// 解析 session （使用Redis）
+	let needSetCookie = false
+	let userId = req.cookie.userId
+	if (userId) {
+		if (!SESSION_DATA[userId]) {
+			SESSION_DATA[userId] = {}
+		}
+	} else {
+		userId = `${Date.now}_${Math.random()}`
+		SESSION_DATA[userId] = {}
+	}
+	req.session = SESSION_DATA[userId]
+	// if (!userId) {
+	// 	needSetCookie = true
+	// 	userId = `${Date.now}_${Math.random()}`
+	// }
+
+	// // 获取 session
+	// req.sessionId = userId
+
+	// console.log('req.session', req.session);
+
 	// 处理 post data
 	getPostData(req).then(postData => {
 		req.body = postData
@@ -46,13 +92,24 @@ const handleServer = (req, res) => {
 		// 处理 blog 路由
 		const blogResult = handleBlogRouter(req, res)
 		if (blogResult) {
-			return blogResult.then(blogData => res.end(JSON.stringify(blogData)))
+			return blogResult.then(blogData => {
+				if (needSetCookie) {
+					res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+				}
+
+				res.end(JSON.stringify(blogData))
+			})
 		}
 
 		// 处理 user 路由
 		const userResult  = handleUserRouter(req, res)
 		if (userResult) {
-			return userResult.then(userData => res.end(JSON.stringify(userData)))
+			return userResult.then(userData => {
+				if (needSetCookie) {
+					res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+				}
+				res.end(JSON.stringify(userData))
+			})
 		}
 
 		res.writeHead(404, { 'Content-type': 'text/plain' })
--- src/router/user.js
+++ src/router/user.js
@@ -1,20 +1,36 @@
-const { loginCheck } = require('../controller/user')
+const { login } = require('../controller/user')
 const { SucessModel, ErrorModel } = require('../model/resModel')
 
 const handleUserRouter = (req, res) => {
   const { method, path } = req
 
-  if (method === 'POST' && path === '/api/blog/login') {
-    const { username, password } = req.body
-    const result = loginCheck(username, password)
+  if (method === 'GET' && path === '/api/blog/login') {
+    const { username, password } = req.query
+    // const { username, password } = req.body
+    const result = login(username, password)
 
     return result.then(data => {
       if (data.username) {
+        // 操作 cookie
+        res.setHeader('Set-Cookie', `username=${data.username}; path=/`)
         return new SucessModel()
       } else {
         return new ErrorModel('登录失败')
       }
     })
+  }
+
+  if (method === 'GET' && path === '/api/blog/login-test') {
+    console.log('req', req.cookie);
+    if (req?.cookie?.username) {
+      return Promise.resolve(
+        new SucessModel({
+          session: req.session
+        })
+      )
+    }
+
+    return Promise.resolve(new ErrorModel('登录失败'))
   }
 }
 
